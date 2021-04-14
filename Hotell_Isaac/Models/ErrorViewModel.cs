a3e90d91cca297e3ddf8eba1d194e246d05242be
Sina Sultani <sinasultani92@gmail.com>
2021-04-14 14:22:11 +0200

    Add project files.

---
+++ Hotell_Isaac/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hotell_Isaac.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
